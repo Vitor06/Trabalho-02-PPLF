4b286bc189dd5dee285a7553b0cfb3e0c6c24e07
Vitor Fernandes <90469090+Vitor06@users.noreply.github.com>
2023-03-17 16:02:54 -0300

    Update doencas.pl

--- doencas.pl
+++ doencas.pl
@@ -8,43 +8,43 @@ sintoma(doresArticulacoes,[dengue,zika]).
 sintoma(dorDeCabeça,[chikungunya,febreAmarela,covid,zika,febreMaculosaBrasileira]).
 sintoma(erupcaoAvermelhadaPele,chikungunya).
 
-sintoma(perdaDeApetite,febreAmarela).
-sintoma(dorMuscular,febreAmarela).
-sintoma(febre,febreAmarela).
-sintoma(nausea,febreAmarela).
-sintoma(vomito,febreAmarela).
+sintoma(perdaDeApetite,[febreAmarela]).
+sintoma(dorMuscular,[febreAmarela]).
+sintoma(febre,[febreAmarela]).
+sintoma(nausea,[febreAmarela]).
+sintoma(vomito,[febreAmarela]).
 
-sintoma(contraturasMusculares,tetanoAcidental).
-sintoma(rigidezBraços,tetanoAcidental).
-sintoma(rigidezPernas,tetanoAcidental).
-sintoma(rigidezAbdominal,tetanoAcidental).
-sintoma(dificuldadeAbrirBoca,tetanoAcidental).
+sintoma(contraturasMusculares,[tetanoAcidental]).
+sintoma(rigidezBraços,[tetanoAcidental]).
+sintoma(rigidezPernas,[tetanoAcidental]).
+sintoma(rigidezAbdominal,[tetanoAcidental]).
+sintoma(dificuldadeAbrirBoca,[tetanoAcidental]).
 
-sintoma(calafrios,covid).
-sintoma(dorDeGarganta,covid).
-sintoma(tosse,covid).
-sintoma(coriza,covid).
-sintoma(faltaDeAr,covid).
-sintoma(coriza,covid).
+sintoma(calafrios,[covid]).
+sintoma(dorDeGarganta,[covid]).
+sintoma(tosse,[covid]).
+sintoma(coriza,[covid]).
+sintoma(faltaDeAr,[covid]).
+sintoma(coriza,[covid]).
 
-sintoma(coceira,zika).
-sintoma(vermelhidaoOlhos,zika).
+sintoma(coceira,[zika]).
+sintoma(vermelhidaoOlhos,[zika]).
 
-sintoma(diarreia,febreMaculosaBrasileira).
-sintoma(dorCostas,febreMaculosaBrasileira).
+sintoma(diarreia,[febreMaculosaBrasileira]).
+sintoma(dorCostas,[febreMaculosaBrasileira]).
 
-sintoma(tremores,parkinson).
-sintoma(lentidao,parkinson).
+sintoma(tremores,[parkinson]).
+sintoma(lentidao,[parkinson]).
 
-sintoma(apneia,obesidade).
-sintoma(ansiedade,obesidade).
-sintoma(depressao,obesidade).
+sintoma(apneia,[obesidade]).
+sintoma(ansiedade,[obesidade]).
+sintoma(depressao,[obesidade]).
 
-sintoma(perdaDeMemoria,alzheimer).
-sintoma(depressao,alzheimer).
-sintoma(desorientacão,alzheimer).
-sintoma(confusao,alzheimer).
-sintoma(agressividade,alzheimer).
+sintoma(perdaDeMemoria,[alzheimer]).
+sintoma(depressao,[alzheimer]).
+sintoma(desorientacão,[alzheimer]).
+sintoma(confusao,[alzheimer]).
+sintoma(agressividade,[alzheimer]).
 
 input([],[]).
 count([],K,0).
@@ -61,23 +61,33 @@ countAll([K|Resto],Lista,[T0|L]):-
 input([SINTOMA|RESTO],Lista):-
     sintoma(SINTOMA,T)-> append(T,T1,Lista), input(RESTO,T1); input(RESTO,Lista).
 
-dividir([],T,[]).
-dividir([X|Resto],Total,[T|L]):-
-	T is (X /Total)*100,
-	dividir(Resto,Total,L).
+calcular([],T,[],[]).
+calcular([X|Resto],Total,[T|L],[Peso|RestoPeso]):-
+	T is (X /Total)*Peso*100,
+	calcular(Resto,Total,L,RestoPeso).
+
+ordenar([],X,[]).
+ordenar([P|R],ProbabilidadesAux,[I|K]):-
+	sort(0,@>=,ProbabilidadesAux,O),
+	nth0(I,O,P),
+	ordenar(R,ProbabilidadesAux,K).
 
 resultado([],[]).
-resultado([doenca|Resto],[X|R]):-
-	write(doenca), write(X),nl,
+resultado([Doenca|Resto],[X|R]):-
+
+	write(Doenca),write(->), write(X), write("%"),nl,nl,
 	resultado(Resto,R).
 
 
-main(Sintomas,Lista,R,P):-
+main(Sintomas,Lista,P):-
+     Pesos = [0.886,0.178,0.00573,0.001,0.002,0.056,0.1,0.4,0.01,0.49],
      input(Sintomas,Lista),
      countAll([dengue,chikungunya,zika,febreMaculosaBrasileira,febreAmarela,tetanoAcidental,alzheimer,obesidade,parkinson,covid],Lista,R),
      sumlist(R,Total),
-     dividir(R,Total,P).
-     %%resultado([dengue,chikungunya,febreMaculodaBrasileira,febreAmarela,tetanoAcidental,alzheimer,obesidade,parkinson,covid],P).
+     calcular(R,Total,P,Pesos),
+     resultado([dengue,chikungunya,zika,febreAmarela,febreMaculodaBrasileira,tetanoAcidental,alzheimer,obesidade,parkinson,covid],P).
+
+
 
 
 
